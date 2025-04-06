86d5f627b49b8e36968ce9e5e69ab40d8e38aefb
MARKO EHMAN <marko.ehman@gmail.com>
2025-04-06 16:20:57 +0200

    task 3

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const path = require('path');
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public')); 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.send("Pozdrav s Railway servera!");
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/gallery', (req, res) => {
+    const dataPath = path.join(__dirname, 'images.json');
+    res.render('slike', { title: 'Galerija slika' });
 });
 
 app.listen(PORT, () => {
-    console.log('Server pokrenut na portu ${PORT}');
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
